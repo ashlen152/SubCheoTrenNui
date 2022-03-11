069b9f4738e58a9327364c58440c63ef7ebe5c85
thinh <thinhsofa@gmail.com>
2022-03-11 16:21:44 +0700

    add short video for intro background

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 
 const NavItem = props => (
-  <li className="px-4 py-6 hover:bg-slate-500">
+  <li className="px-2 py-4 hover:bg-slate-500 cursor-pointer">
     <a href={props.href}>
       {props.text}
     </a>
@@ -10,17 +10,19 @@ const NavItem = props => (
 export default function Home() {
   return (
     <div>
-    <nav className='flex flex-col text-center'>
-      <div>
-        Chèo Trên Núi
-        </div> 
-        <ul className='flex flex-row items-center justify-center gap-4'>
-          <NavItem href='#Product' text="Presentation"></NavItem>
-          <NavItem href='#Product' text="Test"></NavItem>
-          <NavItem href='#Product' text="test"></NavItem>
-          <NavItem href='#Product' text="Test"></NavItem>
+      <nav className='fixed w-full top-4 mx-auto flex flex-col text-center z-50'>
+        <div className="bg-black bg-opacity-20 font-Qwitcher text-8xl p-2 ">
+          Chèo Trên Núi
+        </div>
+        <ul className='bg-black bg-opacity-20 mt-2 flex flex-row items-center justify-center gap-2'>
+          <NavItem href='#Product' text="Giới Thiệu"></NavItem>
+          <NavItem href='#Product' text="Hình Ảnh"></NavItem>
+          <NavItem href='#Product' text="Liên Lạc"></NavItem>
         </ul>
-      </nav> 
+      </nav>
+      <video className='object-cover w-full h-screen z-0' autoplay='true' playsinline='true' muted='true' loop>
+        <source src="intro-short.mov" type="video/mp4" />
+      </video>
     </div>
   )
 
