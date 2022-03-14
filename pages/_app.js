2f6b8a752cd867671c41addf723fe5dbca6baab1
thinh <thinhsofa@gmail.com>
2022-03-14 15:12:31 +0700

    optimize jpg and mp4

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import React from 'react'
 import '../styles/globals.css'
+import Head from 'next/head'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (<>
+    <Head>
+      <title>Chèo Trên Núi</title>
+    </Head>
+    <Component {...pageProps} />
+  </>)
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import { useEffect, useState } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
+import NextImage from 'next/image'
+const videoPoster = 'intro.jpg'
 
 const NavItem = props => (
   <li className="px-2 py-4 hover:bg-black hover:bg-opacity-50 hover:text-white transition-all duration-300 cursor-pointer">
@@ -8,29 +10,50 @@ const NavItem = props => (
   </li>
 )
 
-const mobileCheck = () => {
-  let check = false;
-  (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
-  return check;
-};
+const Loading = () => {
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto">
+        <NextImage  width='200px' height='69px' src='/banner.png' alt='banner' />
+      </div>
+
+    </div>)
+}
 
 export default function Home() {
-  return (
-    <div>
-      <nav className='fixed w-full top-4 mx-auto flex flex-col text-center z-50'>
-        <div className="bg-black bg-opacity-20 font-Qwitcher text-8xl p-2 ">
-          Chèo Trên Núi
-        </div>
-        <ul className='bg-black bg-opacity-20 mt-2 flex flex-row items-center justify-center gap-2'>
-          <NavItem href='#Product' text="Giới Thiệu"></NavItem>
-          <NavItem href='#Product' text="Hình Ảnh"></NavItem>
-          <NavItem href='#Product' text="Liên Lạc"></NavItem>
-        </ul>
-      </nav>
-      <video className='object-cover w-full h-screen z-0' poster="intro.png" autoPlay playsInline muted loop>
-        <source src="intro_720p.mp4" type="video/mp4" />
-      </video>
-    </div>
-  )
+  const videoRef = useRef()
+  const [isLoading, setIsLoading] = useState(true)
 
+  useEffect(() => {
+    const image = new Image()
+    image.src = videoPoster
+    image.onload = () => {
+      setIsLoading(false)
+    }
+  }, [])
+
+  return (
+    <>
+      {
+        isLoading ?
+          <Loading />
+          :
+          <div>
+            <nav className='fixed w-full top-4 mx-auto flex flex-col text-center z-50'>
+              <div className="backdrop-blur-sm bg-black bg-opacity-20 font-Qwitcher text-8xl p-2 ">
+                Chèo Trên Núi
+              </div>
+              <ul className='backdrop-blur-sm bg-black bg-opacity-20 mt-2 flex flex-row items-center justify-center gap-2'>
+                <NavItem href='#Product' text="Giới Thiệu"></NavItem>
+                <NavItem href='#Product' text="Hình Ảnh"></NavItem>
+                <NavItem href='#Product' text="Liên Lạc"></NavItem>
+              </ul>
+            </nav>
+            <video preload="none" ref={videoRef} className='object-cover w-full h-screen z-0' poster={videoPoster} autoPlay playsInline muted loop>
+              <source src="intro_short.mp4" type="video/mp4" />
+            </video>
+          </div>
+      }
+    </>
+  )
 }
