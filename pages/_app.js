41ee044c2408675c45aacc094857010ccaa86cad
thinh <thinhsofa@gmail.com>
2022-03-24 18:29:38 +0700

    add smooth scroll

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../styles/globals.css'
 import Head from 'next/head'
 import { Footer, Navbar } from '../components'
-
+import smoothscroll from 'smoothscroll-polyfill'
 
 function MyApp({ Component, pageProps }) {
-
+  useEffect(() => {
+    smoothscroll.polyfill();
+  }, [])
   return (<>
     <Head>
       <title>Chèo Trên Núi</title>
